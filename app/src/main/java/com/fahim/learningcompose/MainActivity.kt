b9e1ef6ef03773116891a9a360278e24d3f72eaf
ifahimkhan <khan.fahim95@hotmail.com>
2024-12-03 00:06:18 +0530

    if applied this commit will add state hoisting in compose

--- app/src/main/java/com/fahim/learningcompose/MainActivity.kt
+++ app/src/main/java/com/fahim/learningcompose/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -18,6 +19,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,16 +31,28 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LearningComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
             }
         }
     }
 }
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    var shouldShowOnboarding by remember {
+        mutableStateOf(true)
+    }
+    Scaffold(modifier = modifier) { innerPadding ->
+
+        if (shouldShowOnboarding) OnBoardingScreen(onContinueClicked = {
+            shouldShowOnboarding = false
+        }, modifier = modifier) else Greeting(
+            name = "Android",
+            modifier = modifier.padding(innerPadding)
+        )
+    }
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -51,9 +65,11 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         }
         val extraPadding = if (expanded) 48.dp else 0.dp
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(bottom = extraPadding)
+            ) {
                 Text(text = "Hello ")
                 Text(text = name)
             }
@@ -66,10 +82,51 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     }
 }
 
+
+@Composable
+fun OnBoardingScreen(onContinueClicked: () -> Unit, modifier: Modifier = Modifier) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "Welcome to state hoisting ")
+            ElevatedButton(
+                onClick = { onContinueClicked() }
+            ) {
+                Text("continue")
+            }
+        }
+
+
+    }
+}
+
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnBoardingPreview() {
+    LearningComposeTheme {
+        OnBoardingScreen(onContinueClicked = {})
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LearningComposeTheme {
         Greeting("Android")
+    }
+}
+
+@Preview
+@Composable
+private fun MyAppPreview() {
+    LearningComposeTheme {
+        MyApp()
     }
 }
