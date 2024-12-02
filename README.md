fbd8ad6aeaad5b75d8ef11bb2b17a6b8c99e9e62
ifahimkhan <khan.fahim95@hotmail.com>
2024-12-02 23:20:04 +0530

    if applied this commit will add state in compose

---
+++ README.md
@@ -0,0 +1,51 @@
+# Jetpack Compose Basic Android App
+Learn to build an App in jetpack compose - Open-Source Learning Project
+
+
+
+<br>
+
+## You'll build an app with an onboarding screen, and a list of animated expanding items:
+
+## Prerequisites
+* Experience with Kotlin syntax, including lambdas
+
+## What you'll do
+* In this branch, you will learn:
+
+* What Compose is
+* How to build UIs with Compose
+* How to manage state in composable functions
+* How to create a performant list
+* How to add animations
+* How to style and theme an app
+
+
+## Screenshots from this project
+
+
+## Building the project
+* Every feature is easy to follow.
+* Clone the project,see all the branch.
+* To learn and implement from the beginning, start with a new project.
+
+### Find this project useful ? :heart:
+
+* Support it by clicking the :star: button on the upper right of this page. :v:
+
+### License
+```
+   Copyright (C) 2024 Mohammed Fahim Khan
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+```
--- app/src/main/java/com/fahim/learningcompose/MainActivity.kt
+++ app/src/main/java/com/fahim/learningcompose/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fahim.learningcompose.ui.theme.LearningComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +42,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        var expanded by remember {
+            mutableStateOf(false)
+        }
+        val extraPadding = if (expanded) 48.dp else 0.dp
+        Row(modifier = Modifier.padding(24.dp)) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)) {
+                Text(text = "Hello ")
+                Text(text = name)
+            }
+            ElevatedButton(
+                onClick = { expanded = !expanded }
+            ) {
+                if (expanded) Text("Show less") else Text("Show more")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
