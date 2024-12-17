5e5d77a6235264fbd007f775cd6265b398b6712f
ifahimkhan <khan.fahim95@hotmail.com>
2024-12-17 18:52:54 +0530

    if applied this commit will explain State recomposition and working of remember

--- app/src/main/java/com/fahim/learningcompose/ui/screen/WellnessScreen.kt
+++ app/src/main/java/com/fahim/learningcompose/ui/screen/WellnessScreen.kt
@@ -1,12 +1,14 @@
 package com.fahim.learningcompose.ui.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
@@ -28,13 +30,26 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     }
     Column(modifier = modifier.padding(16.dp)) {
         if (count > 0) {
+            var showTask by remember {
+                mutableStateOf(true)
+            }
+            if (showTask) {
+                WellnessTaskItem(
+                    taskName = "Have you taken your 15 min Walk today?",
+                    onClose = { showTask = false })
+            }
             Text(
                 text = "You've had $count glasses.",
                 modifier = modifier.padding(16.dp)
             )
         }
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
-            Text("Add One")
+        Row {
+            Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+                Text("Add One")
+            }
+            Button(onClick = { count = 0 }, Modifier.padding(top = 8.dp)) {
+                Text(text = "Clear water count")
+            }
         }
     }
 }
