27ec327cf5374cf67043443aea126d10bf75d9b8
ifahimkhan <khan.fahim95@hotmail.com>
2024-12-18 19:24:05 +0530

    if applied this commit will impl state hoisting, Stateless and Stateful

--- app/src/main/java/com/fahim/learningcompose/ui/screen/WellnessScreen.kt
+++ app/src/main/java/com/fahim/learningcompose/ui/screen/WellnessScreen.kt
@@ -1,15 +1,12 @@
 package com.fahim.learningcompose.ui.screen
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -20,37 +17,31 @@ import com.fahim.learningcompose.ui.theme.LearningComposeTheme
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    StatefulCounter(modifier)
 }
 
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier) {
+fun StatefulCounter(modifier: Modifier = Modifier) {
     var count by rememberSaveable {
         mutableIntStateOf(0)
     }
+    StatelessWidget(modifier = modifier, count = count, onIncrement = { count++ })
+}
+
+@Composable
+private fun StatelessWidget(modifier: Modifier, count: Int, onIncrement: () -> Unit) {
     Column(modifier = modifier.padding(16.dp)) {
         if (count > 0) {
-            var showTask by remember {
-                mutableStateOf(true)
-            }
-            if (showTask) {
-                WellnessTaskItem(
-                    taskName = "Have you taken your 15 min Walk today?",
-                    onClose = { showTask = false })
-            }
             Text(
-                text = "You've had $count glasses.",
-                modifier = modifier.padding(16.dp)
+                text = "You've had $count glasses.", modifier = modifier.padding(16.dp)
             )
         }
-        Row {
-            Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
-                Text("Add One")
-            }
-            Button(onClick = { count = 0 }, Modifier.padding(top = 8.dp)) {
-                Text(text = "Clear water count")
-            }
+
+        Button(onClick = onIncrement, Modifier.padding(top = 8.dp), enabled = count < 10) {
+            Text("Add One")
         }
+
+
     }
 }
 
@@ -58,6 +49,6 @@ fun WaterCounter(modifier: Modifier = Modifier) {
 @Composable
 fun WaterCounterPreview() {
     LearningComposeTheme {
-        WaterCounter()
+        StatefulCounter()
     }
 }
