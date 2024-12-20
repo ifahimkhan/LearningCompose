060b41d99c045d1978a8b1d3eb57265f32bed63d
ifahimkhan <khan.fahim95@hotmail.com>
2024-12-20 17:04:15 +0530

    if applied this commit will add a button event in compose

--- app/src/main/java/com/fahim/learningcompose/MainActivity.kt
+++ app/src/main/java/com/fahim/learningcompose/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.fahim.learningcompose.ui.theme.LearningComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +23,37 @@ class MainActivity : ComponentActivity() {
         setContent {
             LearningComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    WellnessScreen()
                 }
             }
         }
     }
 }
 
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    WaterCounter(modifier)
+}
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    var count = 0
+    Column {
+        Text(
+            text = "You've had $count glasses.",
+            modifier = modifier.padding(16.dp)
+        )
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+            Text("Add one")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun WellnessScreenPreview() {
     LearningComposeTheme {
-        Greeting("Android")
+        WellnessScreen()
     }
 }
