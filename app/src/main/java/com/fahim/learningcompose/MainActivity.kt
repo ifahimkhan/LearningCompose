02c39a430ebb64e85a2bf0ec48c345e96a66c981
ifahimkhan <khan.fahim95@hotmail.com>
2024-12-21 17:49:53 +0530

    if applied this commit will add remember memory in composition

--- app/src/main/java/com/fahim/learningcompose/MainActivity.kt
+++ app/src/main/java/com/fahim/learningcompose/MainActivity.kt
@@ -8,9 +8,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,7 +26,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LearningComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     WellnessScreen()
                 }
             }
@@ -31,20 +38,20 @@ class MainActivity : ComponentActivity() {
 }
 
 
-
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     WaterCounter(modifier)
 }
+
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    var count = 0
+    val count: MutableState<Int> = remember { mutableStateOf(0) }
     Column {
         Text(
-            text = "You've had $count glasses.",
+            text = "You've had ${count.value} glasses.",
             modifier = modifier.padding(16.dp)
         )
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
+        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
     }
