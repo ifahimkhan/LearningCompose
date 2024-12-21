de804ccce99daf64791fe08baeb336dc6eee6a5b
ifahimkhan <khan.fahim95@hotmail.com>
2024-12-21 18:25:50 +0530

    if applied this commit will add remember state in composition handling configuration change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.fahim.learningcompose"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.fahim.learningcompose"
--- app/src/main/java/com/fahim/learningcompose/MainActivity.kt
+++ app/src/main/java/com/fahim/learningcompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
@@ -12,12 +13,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.fahim.learningcompose.ui.WellnessTaskItem
 import com.fahim.learningcompose.ui.theme.LearningComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,22 +45,47 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     WaterCounter(modifier)
-}
+}/*
+@Composable
+fun WaterCounter(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by remember { mutableStateOf(0) }
+        if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { showTask = false },
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
+            Text("You've had $count glasses.")
+        }
+
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }, enabled = count < 10) {
+                Text("Add one")
+            }
+            Button(
+                onClick = { count = 0 },
+                Modifier.padding(start = 8.dp)) {
+                Text("Clear water count")
+            }
+        }
+    }
+}*/
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
-    val count: MutableState<Int> = remember { mutableStateOf(0) }
-    Column {
-        Text(
-            text = "You've had ${count.value} glasses.",
-            modifier = modifier.padding(16.dp)
-        )
-        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
+    Column(modifier = modifier.padding(16.dp)) {
+        var count by rememberSaveable { mutableStateOf(0) }
+        if (count > 0) {
+            Text("You've had $count glasses.")
+        }
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp), enabled = count < 10) {
             Text("Add one")
         }
     }
 }
-
 @Preview(showBackground = true)
 @Composable
 fun WellnessScreenPreview() {
